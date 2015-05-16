2e8fa7c882520de0a06e43b9800e4883bc864d09
Joaquín Musanti <joakomusanti@gmail.com>
2015-05-16 19:04:00 -0300

    Primer commit

---
+++ Negocio/ModuloUsuario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+   public class ModuloUsuario : EntidadNegocio
+    {
+        int IdUsuario { get; set;} 
+        int IdModulo { get; set;} 
+        bool PermiteAlta { get; set;} 
+        bool PermiteBaja { get; set;}
+        bool PermiteModificacion { get; set;} 
+        bool PermiteConsulta { get; set;} 
+   }
+}
---
+++ Negocio/Usuario.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+  public  class Usuario : EntidadNegocio
+    {
+      string NombreUsuario { get; set; }
+      string Clave { get; set; }
+      string Nombre { get; set; }
+      string Apellido { get; set; }
+      string Email { get; set; }
+      bool Habilitado { get; set; }
+      
+    }
+}
