b86733cdc5a9e12a3b32cdb4a99340d87948284a
emicortez <emi_cortez@hotmail.com>
2015-07-15 17:18:17 -0300

    agregado loguin!

---
+++ UI.Desktop/FormLogin.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Business.Entities;
+using Business.Logic;
+
+namespace UI.Desktop
+{
+    public partial class FormLogin : Form
+    {
+        private Usuario UsuarioActual { get; set; }
+        public FormLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIngresar_Click(object sender, EventArgs e)
+        {
+            UsuarioLogic usr = new UsuarioLogic();
+
+            try
+            {
+                UsuarioActual = usr.GetOneUser(this.txtUsuario.Text);
+            }
+            catch (Exception a)
+            {
+                MessageBox.Show("Error inesperado", a.Message + "\nIntente realizar la operacion nuevamente",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            
+            if (this.txtPass.Text == UsuarioActual.Clave)
+            {
+                this.DialogResult = DialogResult.OK;
+            }
+            else
+            {
+                MessageBox.Show("Usuario y/o contraseña incorrectos", "Login"
+                    , MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void lnkOlvidaPass_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            MessageBox.Show("Es Ud. un usuario muy descuidado, haga memoria", "Olvidé mi contraseña",
+                 MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+
+        }
+               
+    }
+}
