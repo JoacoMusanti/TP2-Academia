c85d094e6130c94ac1baf1e5f56c01b9fc6f013f
andres1790 <andresdlg21@gmail.com>
2015-08-18 09:16:57 -0300

    me habia faltado comitear la ultima version de materias... este tiene los metodos mapearAdatos y mapearDeDatos

--- UI.Desktop/MateriaDesktop.cs
+++ UI.Desktop/MateriaDesktop.cs
@@ -16,12 +16,16 @@ namespace UI.Desktop
     {
 
         private List<Plan> _planes;
+        private List<Especialidad> _especialidades;
+
         public MateriaDesktop()
         {
             InitializeComponent();
             PlanLogic planLogic = new PlanLogic();
             _planes = planLogic.GetAll();
+            CargarEspecialidades();
             CargarPlanes();
+            
         }
 
         private Materia materiaActual { get; set; }
@@ -29,6 +33,17 @@ namespace UI.Desktop
         public MateriaDesktop(ModoForm modo):this()
         {
             Modo = modo;
+        }
+
+        private void CargarEspecialidades()
+        {
+            EspecialidadLogic especialidadLogic = new EspecialidadLogic();
+            _especialidades = especialidadLogic.GetAll();
+
+            cbEspecialidad.ValueMember = "ID";
+            cbEspecialidad.DisplayMember = "Descripcion";
+            cbEspecialidad.DataSource = _especialidades;
+
         }
 
         private void CargarPlanes()
@@ -56,16 +71,63 @@ namespace UI.Desktop
             }
         }
 
-        public void MapearDeDatos()
+        protected override void MapearDeDatos()
         {
-            
+            txtIDMat.Text = materiaActual.ID.ToString();
+            txtDescMat.Text = materiaActual.Descripcion;
+            txtHsSemMat.Text = materiaActual.HorasSemanales.ToString();
+            txtHsTotMat.Text = materiaActual.HorasTotales.ToString();
+            Plan p = _planes.Find(x => x.ID == materiaActual.IdPlan);
+            cbEspecialidad.SelectedValue = _especialidades.Find(y => y.ID == p.IdEspecialidad).ID;
+            cbPlan.SelectedValue = materiaActual.IdPlan;
+
+            if (Modo == ModoForm.Alta || Modo == ModoForm.Modificacion)
+            {
+                btnAceptar.Text = "Guardar";
+            }
+            else if (Modo == ModoForm.Baja)
+            {
+                
+                txtIDMat.Enabled = false;
+                txtHsSemMat.Enabled = false;
+                txtDescMat.Enabled = false;
+                txtHsTotMat.Enabled = false;
+                cbPlan.Enabled = false;
+                cbEspecialidad.Enabled = false;
+                btnAceptar.Text = "Eliminar";
+            }
+        }
+
+        protected override void MapearADatos()
+        {
+            if (Modo == ModoForm.Alta)
+            {
+                Materia mat = new Materia();              
+                materiaActual = mat;
+                materiaActual.State = BusinessEntity.States.New;
+            }
+            if (Modo == ModoForm.Modificacion)
+            {
+                materiaActual.ID = int.Parse(txtIDMat.Text);
+                materiaActual.State = BusinessEntity.States.Modified;
+            }
+            if (Modo == ModoForm.Baja)
+            {
+                materiaActual.ID = int.Parse(txtIDMat.Text);
+                materiaActual.State = BusinessEntity.States.Modified;
+                materiaActual.Baja = true;
+            }
+
+            materiaActual.HorasSemanales = int.Parse(txtHsSemMat.Text);
+            materiaActual.HorasTotales = int.Parse(txtHsTotMat.Text);
+            materiaActual.Descripcion = txtDescMat.Text;
+            materiaActual.IdPlan = Convert.ToInt32(cbPlan.SelectedValue);
         }
 
         protected override bool Validar()
         {
             string error = "";
             bool retorno = true;
-            int temp;
 
             if(string.IsNullOrWhiteSpace(txtDescMat.Text))
             {
@@ -101,7 +163,20 @@ namespace UI.Desktop
 
         protected override void GuardarCambios()
         {
-           
+            MapearADatos();
+
+
+            MateriaLogic mat = new MateriaLogic();
+            try
+            {
+                mat.Save(materiaActual);
+            }
+            catch (Exception e)
+            {
+                Notificar("Error inesperado", e.Message + "\nIntente realizar la operacion nuevamente",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
         }
 
         private void btnCancelar_Click(object sender, EventArgs e)
